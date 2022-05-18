5db2f0f4767c5aa85f682578d9fb7d3a2fe08693
Thomasitsthomas.xyz <thomath047@gmail.com>
2022-05-18 23:11:41 +0530

    inial setup

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+
+const app = express()
+app.use(cors({
+    origin: 'localhost'
+}));
+
+app.get('/offers', async (req, res) => {
+    try {
+        const response = await axios.get('https://cdn.sixt.io/codingtask/offers.json')
+        res.json(response.data)
+    } catch(err) {
+        res.status(err.code || 500).send('offer api failed')
+    }
+    
+});
+
+
+app.listen(process.env.PORT, () => {
+    console.log("Connected to port process.env.PORT");
+})
